953c2e3a7712a6679e2f97c220432225f8593568
milinpatel07 <49778169+milinpatel07@users.noreply.github.com>
2025-06-10 14:14:24 +0200

    Load class names from config

--- pcdet/datasets/kitti/kitti_dataset.py
+++ pcdet/datasets/kitti/kitti_dataset.py
@@ -429,11 +429,11 @@ if __name__ == '__main__':
         import yaml
         from pathlib import Path
         from easydict import EasyDict
-        dataset_cfg = EasyDict(yaml.load(open(sys.argv[2])))
+        dataset_cfg = EasyDict(yaml.safe_load(open(sys.argv[2])))
         ROOT_DIR = (Path(__file__).resolve().parent / '../../../').resolve()
         create_kitti_infos(
             dataset_cfg=dataset_cfg,
-            class_names=['Car', 'Pedestrian', 'Cyclist'],
+            class_names=dataset_cfg.CLASS_NAMES,
             data_path=ROOT_DIR / 'data' / 'kitti',
             save_path=ROOT_DIR / 'data' / 'kitti'
         )
