d01ee396f3c5bb86bfd504185d5fefef1bfe334d
milinpatel07 <49778169+milinpatel07@users.noreply.github.com>
2025-06-10 14:24:16 +0200

    Adapt temperature scaling script for KITTI

--- pcdet/models/model_utils/__init__.py
+++ pcdet/models/model_utils/__init__.py
@@ -1,4 +1,7 @@
 from .model_nms_utils import class_agnostic_nms
 from .mc_dropout import MonteCarloDropout, MonteCarloDropout2d
+from .dataset_utils import post_processing_var
+from .cluster_utils import preprocess
 
-__all__ = ['class_agnostic_nms', 'MonteCarloDropout', 'MonteCarloDropout2d']
+__all__ = ['class_agnostic_nms', 'MonteCarloDropout', 'MonteCarloDropout2d',
+           'post_processing_var', 'preprocess']
